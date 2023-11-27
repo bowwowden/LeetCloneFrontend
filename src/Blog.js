13b38e09d258140c340b097dd7814d3812b8c91e
william <william.f.bowden@gmail.com>
2023-11-26 22:10:13 -0700

    Added some new files for another blog post, still messy

--- src/Blog.js
+++ src/Blog.js
@@ -1,5 +1,7 @@
 import React from "react";
 import cheetahQuicksort from './assets/cheetah-quicksort.jpg';
+import unionFindBlocks from './assets/blocks-symmetry.jpg';
+
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -29,6 +31,30 @@ const Blog = () => {
           </div>
         </Card.Body>
       </Card>
+
+      {/* Other blog */}
+      <Card className="blogs">
+        <img
+          src={unionFindBlocks}
+          alt="blocks-union-find"
+          className='image'
+        />
+        <Card.Body >
+
+          <Card.Title className="title">Quicksort faster than a cheetah</Card.Title>
+          <Card.Text>
+           Quicksort is one of the fastest algorithms for arrays. However it has its 
+           drawbacks with other data structures ...
+          </Card.Text>
+
+          <br/>
+          <div className="read-more">
+            <Link to="/blog/cheetah-quicksort">Read More</Link>
+          </div>
+        </Card.Body>
+      </Card>
+
+
 
       </div>
     </div>
--- src/blogpages/cheetah-quicksort.js
+++ src/blogpages/cheetah-quicksort.js
@@ -1,19 +1,82 @@
 // CheetahQuicksort.js
 import React from 'react';
+import cheetahpic from '../assets/cheetah-quicksort.jpg';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'; // Import the Dracula theme
+
+const codeExample = `
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+`;
+
 
 const CheetahQuicksort = () => {
   return (
-    <div>
-      <h1>Cheetah Quicksort Page</h1>
-      {/* Add your content here */}
+    <div className="App">
+      
 
-      <p> More content </p>
-      <p> SUCCCESSSS
-        SelectedS
-        S
-        S
-        S
-        </p>
+      <div className="article-bg">
+
+            <div className="article-body">
+            {/* <header className="problem-header"> */}
+            
+            <div className='article-title'>
+
+              <h1>Cheetah Quicksort Page</h1>
+            </div>
+            
+          
+              <img
+                src={cheetahpic}
+                alt="cheetah-quicksort"
+                className='image-article'
+              />
+
+            <br/> 
+            <p>
+              Quicksort is a sorting algorithm that efficiently sorts an array by dividing it into smaller subarrays,
+              recursively sorting the subarrays, and then combining them. Let's explore how quicksort is as fast as a cheetah!
+            </p>
+            <br/>
+            <h2>Algorithm Overview</h2>
+            <p>
+              Quicksort follows the divide-and-conquer strategy. It selects a 'pivot' element from the array and partitions
+              the other elements into two subarrays according to whether they are less than or greater than the pivot.
+              The subarrays are then recursively sorted.
+            </p>
+            <br/> 
+
+            <h2>Why Cheetah?</h2>
+            <p>
+              Quicksort is known for its speed, and comparing it to a cheetah emphasizes its agility and efficiency.
+              The algorithm's average and best-case time complexity is O(n log n), making it one of the fastest sorting algorithms.
+            </p>
+
+            <br/> 
+            <h2>Implementation</h2>
+            <p>
+              The implementation of quicksort involves choosing a pivot, partitioning the array, and recursively sorting
+              the subarrays. The efficiency of quicksort lies in its ability to minimize the number of comparisons and swaps.
+            </p>
+            
+            
+            <SyntaxHighlighter language="python" style={dracula}>
+                {codeExample}
+            </SyntaxHighlighter>
+      
+      
+            <br/> 
+
+            </div>
+
+      </div>
+     
     </div>
   );
 };
